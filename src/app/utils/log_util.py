b0e0a3b6abb47c74e657336063ce4b97996d56f6
julian <m15216818750@163.com>
2024-08-22 17:54:14 +0800

    chore: Update logging configuration to include daily log rotation

--- src/app/utils/log_util.py
+++ src/app/utils/log_util.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import sys
+from logging import handlers
 
 LOG_LEVEL = os.environ.get("SERVICE_LOG_LEVEL", "DEBUG")
 
@@ -10,7 +11,19 @@ default_formatter = logging.Formatter(
 stream_handler = logging.StreamHandler(stream=sys.stderr)
 stream_handler.setLevel(LOG_LEVEL)
 stream_handler.setFormatter(default_formatter)
+
+# file_handler = logging.FileHandler("src/app/logs/service.log")
+# file_handler.setLevel(LOG_LEVEL)
+# file_handler.setFormatter(default_formatter)
+time_rotating_file_handler = handlers.TimedRotatingFileHandler(
+    filename="src/app/logs/daily.log", when="D"
+)
+time_rotating_file_handler.setLevel(LOG_LEVEL)
+time_rotating_file_handler.setFormatter(default_formatter)
+
 logger = logging.getLogger(__name__)
 logger.addHandler(stream_handler)
+logger.addHandler(time_rotating_file_handler)
+
 logger.setLevel(LOG_LEVEL)
 logger.propagate = False
